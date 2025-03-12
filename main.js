9f2888f1028b55b9a462579db3f309a983d1b462
o-mckee <102201903+o-mckee@users.noreply.github.com>
2025-03-12 13:12:09 -0700

    asgn5 push5

--- main.js
+++ main.js
@@ -41,10 +41,10 @@ function main() {
 
     // ADD FLOOR PLANE
     {
-        const planeSize = 40;
+        const planeSize = 60;
  
         const loader = new THREE.TextureLoader();
-        const texture = loader.load('public/stonebrick.jpg');
+        const texture = loader.load('public/snow.jpg');
         texture.wrapS = THREE.RepeatWrapping;
         texture.wrapT = THREE.RepeatWrapping;
         texture.magFilter = THREE.NearestFilter;
@@ -53,7 +53,7 @@ function main() {
         texture.repeat.set(repeats, repeats);
 
         const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-        const planeMat = new THREE.MeshPhongMaterial({
+        const planeMat = new THREE.MeshBasicMaterial({
             map: texture,
             side: THREE.DoubleSide,
         });
@@ -63,25 +63,67 @@ function main() {
     }
 
     // ADD CUBE
-    {
-        const cubeSize = 4;
-        const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-        const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
-        const mesh = new THREE.Mesh(cubeGeo, cubeMat);
-        mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
-        scene.add(mesh);
-    }
+    const cubeSize = 4;
+    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMat = new THREE.MeshPhongMaterial({color: 0x002288});
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+    mesh.position.set(15, 2, 0);
+    mesh.rotation.y = 10;
+    scene.add(mesh);
 
-    function makeCube(cColor) {
+    function makeCube(cColor, posX, posY, posZ) {
         const cSize = 2;
         const cGeo = new THREE.BoxGeometry(cSize, cSize, cSize);
         const cMat = new THREE.MeshPhongMaterial({color: cColor});
         const cMesh = new THREE.Mesh(cGeo, cMat);
-        cMesh.position.y = 1
+        cMesh.position.x = posX * 2;
+        cMesh.position.y = 1 + (posY * 2);
+        cMesh.position.z = posZ * 2;
         scene.add(cMesh);
     }
 
-    makeCube(0x448844); 
+    function makeCubeSize(cColor, posX, posY, posZ, sizeX, sizeY, sizeZ) {
+        const cGeo = new THREE.BoxGeometry(sizeX, sizeY, sizeZ);
+        const cMat = new THREE.MeshPhongMaterial({color: cColor});
+        const cMesh = new THREE.Mesh(cGeo, cMat);
+        cMesh.position.x = posX * 2;
+        cMesh.position.y = 1 + (posY * 2);
+        cMesh.position.z = posZ * 2;
+        scene.add(cMesh);
+    }
+
+    function makeCubeTexture(posX, posY, posZ, path, transparentBool) {
+        const cSize = 2;
+        const cGeo = new THREE.BoxGeometry(cSize, cSize, cSize);
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load(path);
+        const cMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            transparent: transparentBool
+        });
+        const cMesh = new THREE.Mesh(cGeo, cMat);
+        cMesh.position.x = posX * 2;
+        cMesh.position.y = 1 + (posY * 2);
+        cMesh.position.z = posZ * 2;
+        scene.add(cMesh);
+    } 
+
+    function makeCubeTextureSize(posX, posY, posZ, path, transparentBool, sizeX, sizeY, sizeZ) {
+        const cGeo = new THREE.BoxGeometry(sizeX, sizeY, sizeZ);
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load(path);
+        const cMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            transparent: transparentBool
+        });
+        const cMesh = new THREE.Mesh(cGeo, cMat);
+        cMesh.position.x = posX * 2;
+        cMesh.position.y = 1 + (posY * 2);
+        cMesh.position.z = posZ * 2;
+        scene.add(cMesh);
+    }
+
+    makeCube(0x448844, 0, 0, 0);
 
       // ADD SPHERE
     {
@@ -89,25 +131,47 @@ function main() {
         const sphereWidthDivisions = 32;
         const sphereHeightDivisions = 16;
         const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
-        const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
+        const sphereMat = new THREE.MeshPhongMaterial({color: 0x882200});
         const mesh = new THREE.Mesh(sphereGeo, sphereMat);
-        mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+        mesh.position.set(-20, 3, -10);
         scene.add(mesh);
     }
 
-    // ADD CYLINDER
+    // ADD CYLINDER (flagpole)
     {
-        const radiusTop = 2;
-        const radiusBot = 2;
-        const cylHeight = 4;
-        const cylRadSegments = 12;
+        const radiusTop = 0.5;
+        const radiusBot = 0.5;
+        const cylHeight = 10;
+        const cylRadSegments = 12; // 25 also works
         const cylGeo = new THREE.CylinderGeometry(radiusTop, radiusBot, cylHeight, cylRadSegments);
-        const cylMat = new THREE.MeshPhongMaterial({color: 0x884488});
+        const cylMat = new THREE.MeshPhongMaterial({color: 0x662200});
         const cylMesh = new THREE.Mesh(cylGeo, cylMat);
-        cylMesh.position.y = 1;
-        cylMesh.position.x = 15;
+        cylMesh.position.z = -18;
+        cylMesh.position.y = 13;
+        cylMesh.position.x = -7;
         scene.add(cylMesh);
     }
+
+    // ADD FLAG
+
+    const flagSize1 = 5;
+    const flagSize2 = 3;
+    const flagWidth = 0.5;
+    const flagGeo = new THREE.BoxGeometry(flagSize1, flagSize2, flagWidth);
+    const flagMat = new THREE.MeshPhongMaterial({color: 0x222288});
+    const flagMesh = new THREE.Mesh(flagGeo, flagMat);
+    flagMesh.position.set(-4,16,-18);
+    scene.add(flagMesh);
+
+    // ADD FLAGPOLE SPHERE
+    const sphereRadius = 0.75;
+    const sphereWidthDivisions = 32;
+    const sphereHeightDivisions = 16;
+    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+    const sphereMat = new THREE.MeshPhongMaterial({color: 0xaa9900});
+    const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+    sphereMesh.position.set(-7, 18, -18);
+    scene.add(sphereMesh);
 
     // ADD FISH
     // add 3d model (.obj)
@@ -125,12 +189,7 @@ function main() {
         objLoader.load('public/fish.obj', (fishObjRoot) => {
             fishObjRoot.scale.setScalar(2);
             scene.add(fishObjRoot);
-            fishObjRoot.position.set(3, 0, -2);
-        });
-
-        objLoader.load('public/hat.obj', (hatObjRoot) => {
-            hatObjRoot.scale.setScalar(2);
-            scene.add(hatObjRoot);
+            fishObjRoot.position.set(-15, 5, 13);
         });
     }
 
@@ -154,6 +213,81 @@ function main() {
         folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
         folder.open();
     }
+
+    // MAKE STONE BRICK BUILDING
+    {
+
+        // floor
+        for (let i = -10; i < -4; i++) {
+            for (let j = -5; j < 5; j++) {
+                makeCubeTexture(j, 0, i, 'public/stonebrick.jpg');
+            }
+        }
+
+        // back
+        for (let i = 1; i <= 3; i++) {
+            for (let j = -5; j < 5; j++) {
+                makeCubeTexture(j, i, -10, 'public/stonebrick.jpg');
+                if (i == 1 && Math.abs(j) % 2 == 1) {
+                    makeCubeTexture(j, 4, -10, 'public/stonebrick.jpg');
+                }
+            }
+        }
+    
+        // front
+        for (let i = 1; i <= 2; i++) {
+            for (let j = -5; j < 5; j++) {
+                if (j != -1) { // door space
+                    makeCubeTexture(j, i, -5, 'public/stonebrick.jpg');
+                }
+                if (i == 1) {
+                    makeCubeTexture(j, 3, -5, 'public/stonebrick.jpg'); // full line
+                    if (Math.abs(j) % 2 == 0) { // uneven placement
+                        makeCubeTexture(j, 4, -5, 'public/stonebrick.jpg');
+                    }
+                }
+
+            }
+        }
+
+        // left side
+        for (let i = 1; i <= 2; i++) {
+            for (let j = -9; j < -5; j++) {
+                makeCubeTexture(-5, i, j, 'public/stonebrick.jpg');
+                if (i == 1) {
+                    makeCubeTexture(-5, 3, j, 'public/stonebrick.jpg'); // full line
+                    if (Math.abs(j) % 2 == 0) {
+                        makeCubeTexture(-5, 4, j, 'public/stonebrick.jpg');
+                    }
+                }
+            }
+        }
+
+        // right side
+        for (let i = 1; i <= 2; i++) {
+            for (let j = -9; j < -5; j++) {
+                makeCubeTexture(4, i, j, 'public/stonebrick.jpg');
+                if (i == 1) {
+                    makeCubeTexture(4, 3, j, 'public/stonebrick.jpg'); // full line
+                    if (Math.abs(j) % 2 == 1) {
+                        makeCubeTexture(4, 4, j, 'public/stonebrick.jpg');
+                    }
+                }
+            }
+        }
+
+        // ceiling
+        for (let i = -9; i < -5; i++) {
+            for (let j = -4; j < 4; j++) {
+                makeCubeTexture(j, 3, i, 'public/stonebrick.jpg');
+            }
+        }
+
+    
+    }
+
+    // FISHTANK
+    makeCubeTextureSize(-10, 2, 7, 'public/ice.png', true, 11, 11, 11);
 
     // ADD LIGHTS AND LIGHT GUI
     {
@@ -181,14 +315,12 @@ function main() {
         scene.add(pointLight)
 
         // point light mesh
-        {
-            const pointSize = 1;
-            const pointGeo = new THREE.BoxGeometry(pointSize, pointSize, pointSize);
-            const pointMat = new THREE.MeshBasicMaterial({color: 0xFFF933});
-            const pointMesh = new THREE.Mesh(pointGeo, pointMat);
-            pointMesh.position.set(10, 10, -10);
-            scene.add(pointMesh);
-        }
+        const pointSize = 1;
+        const pointGeo = new THREE.BoxGeometry(pointSize, pointSize, pointSize);
+        const pointMat = new THREE.MeshBasicMaterial({color: 0xFFF933});
+        const pointMesh = new THREE.Mesh(pointGeo, pointMat);
+        pointMesh.position.set(10, 10, -10);
+        scene.add(pointMesh);
 
 
         const helper = new THREE.DirectionalLightHelper(light);
@@ -220,12 +352,18 @@ function main() {
         return needResize;
     }
 
-    function render() {
+    function render(time) {
+
+        time *= 0.001;
+        //pointMesh.rotation.z = time;
+        
+
         if (resizeRenderer(renderer)) {
             const canvas = renderer.domElement;
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
         }
+
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
